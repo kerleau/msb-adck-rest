1293b1c0afc8cf4cb234965d21fb8092d9c9587f
k7k <k7k@metta-sati.fritz.box>
2023-03-19 15:02:14 +0100

    Add code stage 1

---
+++ src/main/java/alex/springboot/App.java
@@ -0,0 +1,12 @@
+package alex.springboot;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web-controller/src/test/java/alex/springboot/AppTest.java
@@ -0,0 +1,13 @@
+package alex.springboot;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    @DisplayName("Test controller should return true")
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
